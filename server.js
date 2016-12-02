e1eb4bd7c803cc43240ea1fc6d1f6b2c253e6426
Joseph <joseph.jr@gmail.com>
2016-12-02 14:16:51 +0000

    change server.js

--- server.js
+++ server.js
@@ -1,37 +1,31 @@
-var express    = require('express');
-var mongoskin = require('mongoskin')
+
+var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
+// configure app to use bodyParser()
+// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var db = mongoskin.db('mongodb://localhost:27017/test', {safe:true});
+var port = process.env.PORT || 8080;        // set our port
 
-var port = process.env.PORT || 3000
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
 
-app.param('collectionName', function(req, res, next, collectionName){
-  req.collection = db.collection(collectionName)
-  return next()
-} )
-
-app.get('/transactions', function(req, res){
-  res.send('Welcome, please select a transaction')
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
-app.get('/transactions/:collectionName', function(req, res){
-  req.collection.find({}, {limit:10, sort: [['_id, -1']]}).toArray(function(err, results){
-    if (err) return next(err)
-    res.send(results)
-   })
-});
+// more routes for our API will happen here
 
-app.post('/transactions/:collectionName', function(req, res){
-  req.collection.insert(req.body, {}, function(err, results){
-    if (err) return next(err)
-    res.send(results)
-  })
-});
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
 
+// START THE SERVER
+// =============================================================================
 app.listen(port);
-console.log('Server running on port '+ port);
+console.log('Magic happens on port ' + port);
